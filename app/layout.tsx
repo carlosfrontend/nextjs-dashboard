0604d6433839d1dd796539665fc9054fc7c21de8
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-03-28 12:01:37 +0100

    Feat: Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { inter } from './ui/fonts';
+import './ui/global.css';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+       {children}
+       <footer className='bg-blue-500 text-white p-10 flex justify-center'>
+        <p>Hecho con amor &#x1F498; por @carlosfrontend</p>
+        
+       </footer>
+      </body>
     </html>
   );
 }
